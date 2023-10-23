0abbb76261d2bac405cf91cea85dbc5aac3144e5
Jennifer Stewart <jstewart8053@gmail.com>
2023-10-23 00:48:37 -0400

    added components

---
+++ src/components/Directions.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Directions() {
+  return (
+    <div className="col">
+      <img
+        src="/assets/directions.svg"
+        alt="directions"
+        style={{ width: "100vw" }}
+      />
+    </div>
+  );
+}
+export default Directions;
---
+++ src/components/Hero.js
@@ -0,0 +1,10 @@
+import React from "react";
+function Hero() {
+  return (
+    <div className="col">
+      <img src="/assets/hero.gif" alt="hero" />
+    </div>
+  );
+}
+
+export default Hero;
