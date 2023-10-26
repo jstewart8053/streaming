299c537fbf2b43fd4bfe7e9a80b3822e07a879a6
Jennifer Stewart <jstewart8053@gmail.com>
2023-10-25 23:47:03 -0400

    made searchbar functional

--- src/App.js
+++ src/App.js
@@ -1,15 +1,79 @@
 import "./App.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Directions from "./components/Directions";
 import Hero from "./components/Hero";
-import Searchbar from "./components/Searchbar";
 import Cards from "./components/Cards";
+
 function App() {
+  const [results, setResults] = useState([]);
+  const [query, setQuery] = useState("Matrix");
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    getResults();
+  }, [query]);
+
+  const fetchData = async () => {
+    const options = {
+      method: "GET",
+      url: "https://streaming-availability.p.rapidapi.com/search/title",
+      params: {
+        title: query,
+        country: "us",
+        show_type: "all",
+        output_language: "en",
+      },
+
+      headers: {
+        "X-RapidAPI-Key": "d72a7fd318msha82e97ad3e6671dp1e4dd0jsna89c7cd4e107",
+        "X-RapidAPI-Host": "streaming-availability.p.rapidapi.com",
+      },
+    };
+
+    try {
+      const response = await axios(options);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getResults = async () => {
+    const response = await fetchData();
+  };
+
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  };
+
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
   return (
     <>
       <div className="App">
         <Hero />
         <Directions />
-        <Searchbar />
+        <div className="searchContainer">
+          <div className="searchbar">
+            <form onSubmit={getSearch} className="search-form">
+              <input
+                className="searchBar"
+                type="text"
+                value={search}
+                onChange={updateSearch}
+              />
+
+              <button className="search-button" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
       <div className="cardContainer">
         <Cards />
